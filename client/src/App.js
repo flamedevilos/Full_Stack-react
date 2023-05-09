7024ba7de186f13865567899e2df0d9c86fdc530
Chakib Mastouri <38005501+flamedevilos@users.noreply.github.com>
2023-05-09 19:48:38 +0200

    client side

---
+++ client/src/App.js
@@ -0,0 +1,27 @@
+//import logo from "./logo.svg";
+import "./App.css";
+import React, { useState, useEffect } from "react";
+
+function App() {
+  const [backendData, setBackendData] = useState([{}]);
+
+  useEffect(() => {
+    fetch("/api")
+      .then((response) => response.json())
+      .then((data) => {
+        setBackendData(data);
+      });
+  }, []);
+  return (
+    <div className="App">
+      <h1>Backend goes here!</h1>
+      {typeof backendData.users === "undefined" ? (
+        <p>Loading...</p>
+      ) : (
+        backendData.users.map((user, i) => <p key={i}>{user}</p>)
+      )}
+    </div>
+  );
+}
+
+export default App;
